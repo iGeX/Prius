8e814de3aa41fc0bce7fa495538b38cca148ac30
Румянцев Алексей <RumyantsevAN@tnt.transneft.ru>
2026-04-19 21:25:57 +0800

    NuspecMapper

---
+++ Src/Prius.Core.Packages/NuspecMapper.cs
@@ -0,0 +1,86 @@
+﻿using System.Xml.Linq;
+using Prius.Core.Maps;
+
+namespace Prius.Core.Packages;
+
+public static class NuspecMapper
+{
+    public static DictionaryMap ToMap(string xml)
+    {
+        var doc = XDocument.Parse(xml);
+        var ns = doc.Root?.GetDefaultNamespace() ?? XNamespace.None;
+        var metadata = doc.Root?.Element(ns + "metadata");
+
+        var rootMap = DictionaryMap.New;
+        if (metadata == null) return rootMap;
+
+        var info = DictionaryMap.New;
+        info.Put("Id", metadata.Element(ns + "id")?.Value ?? string.Empty);
+        info.Put("Version", metadata.Element(ns + "version")?.Value ?? string.Empty);
+        info.Put("Authors", metadata.Element(ns + "authors")?.Value ?? string.Empty);
+        info.Put("Description", metadata.Element(ns + "description")?.Value ?? string.Empty);
+        info.Put("ProjectUrl", metadata.Element(ns + "projectUrl")?.Value ?? string.Empty);
+        info.Put("Icon", metadata.Element(ns + "icon")?.Value ?? string.Empty);
+        info.Put("Tags", metadata.Element(ns + "tags")?.Value ?? string.Empty);
+        info.Put("License", metadata.Element(ns + "license")?.Value ?? string.Empty);
+        
+        var repo = metadata.Element(ns + "repository");
+        if (repo != null)
+        {
+            var repoMap = DictionaryMap.New;
+            repoMap.Put("Type", repo.Attribute("type")?.Value ?? string.Empty);
+            repoMap.Put("Url", repo.Attribute("url")?.Value ?? string.Empty);
+            repoMap.Put("Commit", repo.Attribute("commit")?.Value ?? string.Empty);
+            info.Put("Repository", repoMap);
+        }
+        rootMap.Put("Info", info);
+
+        var dependenciesNode = metadata.Element(ns + "dependencies");
+        if (dependenciesNode != null)
+        {
+            var depsMap = DictionaryMap.New;
+            var groups = dependenciesNode.Elements(ns + "group").ToList();
+
+            if (groups.Count > 0)
+            {
+                foreach (var group in groups)
+                {
+                    var tfm = group.Attribute("targetFramework")?.Value ?? "any";
+                    depsMap.Put(tfm, ParseDependencyGroup(group, ns));
+                }
+            }
+            else
+            {
+                depsMap.Put("any", ParseDependencyGroup(dependenciesNode, ns));
+            }
+            rootMap.Put("Dependencies", depsMap);
+        }
+
+        return rootMap;
+    }
+
+    private static DictionaryMap ParseDependencyGroup(XElement container, XNamespace ns)
+    {
+        var groupMap = DictionaryMap.New;
+        foreach (var dep in container.Elements(ns + "dependency"))
+        {
+            var id = dep.Attribute("id")?.Value;
+            if (string.IsNullOrEmpty(id)) continue;
+
+            var depInfo = DictionaryMap.New;
+            depInfo.Put("Version", dep.Attribute("version")?.Value ?? "0.0.0");
+            
+            var include = dep.Attribute("include")?.Value;
+            if (!string.IsNullOrEmpty(include)) depInfo.Put("Include", include);
+
+            var exclude = dep.Attribute("exclude")?.Value;
+            if (!string.IsNullOrEmpty(exclude)) depInfo.Put("Exclude", exclude);
+
+            var privateAssets = dep.Attribute("privateAssets")?.Value;
+            if (!string.IsNullOrEmpty(privateAssets)) depInfo.Put("PrivateAssets", privateAssets);
+
+            groupMap.Put(id, depInfo);
+        }
+        return groupMap;
+    }
+}
---
+++ Tests/Prius.Core.Packages.Tests/NuspecMapperTests.cs
@@ -0,0 +1,66 @@
+﻿using Prius.Core.Maps;
+using Xunit;
+
+namespace Prius.Core.Packages.Tests;
+
+public sealed class NuspecMapperTests
+{
+    [Fact]
+    public void Should_Parse_Full_Nuspec_To_Map()
+    {
+        const string Xml = """
+            <?xml version="1.0" encoding="utf-8"?>
+            <package xmlns="http://microsoft.com">
+              <metadata>
+                <id>Prius.Core</id>
+                <version>1.0.0-beta</version>
+                <authors>iGeX</authors>
+                <description>Core universe logic</description>
+                <repository type="git" url="https://github.com" commit="abcdef" />
+                <dependencies>
+                  <group targetFramework="net8.0">
+                    <dependency id="System.Text.Json" version="8.0.0" exclude="Build,Analyzers" />
+                  </group>
+                  <group targetFramework="netstandard2.1">
+                    <dependency id="Newtonsoft.Json" version="13.0.3" privateAssets="All" />
+                  </group>
+                </dependencies>
+              </metadata>
+            </package>
+            """;
+
+        var map = NuspecMapper.ToMap(Xml);
+
+        Assert.Equal("Prius.Core", map.DeepGet("Info/Id").AsValue<string>());
+        Assert.Equal("1.0.0-beta", map.DeepGet("Info/Version").AsValue<string>());
+        Assert.Equal("git", map.DeepGet("Info/Repository/Type").AsValue<string>());
+        Assert.Equal("abcdef", map.DeepGet("Info/Repository/Commit").AsValue<string>());
+        
+        Assert.Equal("8.0.0", map.DeepGet("Dependencies/net8.0/System.Text.Json/Version").AsValue<string>());
+        Assert.Equal("Build,Analyzers", map.DeepGet("Dependencies/net8.0/System.Text.Json/Exclude").AsValue<string>());
+        
+        Assert.Equal("13.0.3", map.DeepGet("Dependencies/netstandard2.1/Newtonsoft.Json/Version").AsValue<string>());
+        Assert.Equal("All", map.DeepGet("Dependencies/netstandard2.1/Newtonsoft.Json/PrivateAssets").AsValue<string>());
+    }
+
+    [Fact]
+    public void Should_Handle_Old_Format_Without_Groups_As_Any()
+    {
+        const string Xml = """
+            <?xml version="1.0"?>
+            <package xmlns="http://microsoft.com">
+              <metadata>
+                <id>Old.Pkg</id>
+                <version>1.0.0</version>
+                <dependencies>
+                  <dependency id="Lib.A" version="2.0.0" />
+                </dependencies>
+              </metadata>
+            </package>
+            """;
+
+        var map = NuspecMapper.ToMap(Xml);
+
+        Assert.Equal("2.0.0", map.DeepGet("Dependencies/any/Lib.A/Version").AsValue<string>());
+    }
+}
